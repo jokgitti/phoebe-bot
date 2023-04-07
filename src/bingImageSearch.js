d4616d67d30af8647d4d4057a85303206bb45fcd
Luca Galeazzi <luca.galeazzi@jimdo.com>
2023-04-07 22:01:38 +0200

    create bingImageSearch

---
+++ src/bingImageSearch.js
@@ -0,0 +1,29 @@
+const request = require("request");
+
+async function bingImageSearch(query) {
+  const url = new URL("https://api.bing.microsoft.com/v7.0/images/search");
+  const parameters = [
+    ["q", query],
+    ["safeSearch", "off"],
+  ];
+  const urlEncodedParamers = new URLSearchParams(parameters).toString();
+  url.search = urlEncodedParamers;
+
+  const headers = {
+    "Ocp-Apim-Subscription-Key": process.env.BING_SEARCH_API_KEY,
+  };
+
+  return new Promise((resolve, reject) => {
+    request({ url, headers }, (error, _, body) => {
+      const results = JSON.parse(body).value;
+
+      if (error) {
+        reject(error);
+      }
+
+      resolve(results[0] || null);
+    });
+  });
+}
+
+module.exports = bingImageSearch;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const TelegramBot = require("node-telegram-bot-api");
-const request = require("request");
+const bingImageSearch = require("./bingImageSearch");
 
 const token = process.env.TELEGRAM_API_KEY;
 const bot = new TelegramBot(token, { polling: true });
@@ -7,70 +7,48 @@ const bot = new TelegramBot(token, { polling: true });
 console.log(process.env.TELEGRAM_API_KEY);
 console.log(process.env.BING_SEARCH_API_KEY);
 
-const again = {};
-
 async function lookfor(msg, match) {
   console.log(msg);
   const query = match[1];
 
-  const url = new URL("https://api.bing.microsoft.com/v7.0/images/search");
-  const parameters = [
-    ["q", query],
-    ["safeSearch", "off"],
-  ];
-  const urlEncodedParamers = new URLSearchParams(parameters).toString();
-  url.search = urlEncodedParamers;
+  try {
+    const bingImage = await bingImageSearch(query);
+    console.log(bingImage);
 
-  const headers = {
-    "Ocp-Apim-Subscription-Key": process.env.BING_SEARCH_API_KEY,
-  };
-
-  request({ url, headers }, async (error, response, body) => {
-    try {
-      const results = JSON.parse(body).value;
-
-      if (error) {
-        throw error;
-      }
-
-      const firstImage = results[0] || null;
-
-      if (!firstImage) {
-        await bot.sendMessage(
-          msg.chat.id,
-          "Sorry, I couldn't find any images for that query."
-        );
-        return;
-      }
-
-      console.log("encodingFormat", firstImage.encodingFormat);
-
-      switch (firstImage.encodingFormat) {
-        case "animatedgif": {
-          await bot.sendAnimation(msg.chat.id, firstImage.contentUrl, {
-            caption: `here's ${query}`,
-          });
-          break;
-        }
-        default: {
-          await bot.sendPhoto(msg.chat.id, firstImage.contentUrl, {
-            caption: `here's ${query}`,
-          });
-        }
-      }
-    } catch (error) {
-      console.error(error.message || error);
-      bot.sendMessage(msg.chat.id, "something went wrong :(");
+    if (!bingImage) {
+      await bot.sendMessage(
+        msg.chat.id,
+        "Sorry, I couldn't find anything ¯\\_(ツ)_/¯."
+      );
+      return;
     }
-  });
+
+    switch (bingImage.encodingFormat) {
+      case "animatedgif": {
+        await bot.sendAnimation(msg.chat.id, bingImage.contentUrl, {
+          caption: `here's ${query}`,
+        });
+        break;
+      }
+      default: {
+        await bot.sendPhoto(msg.chat.id, bingImage.contentUrl, {
+          caption: `here's ${query}`,
+        });
+      }
+    }
+  } catch (error) {
+    console.error(error.message || error);
+    bot.sendMessage(msg.chat.id, "something went wrong :(");
+  }
 }
 
-bot.onText(/look for (.+)/i, lookfor);
+bot.onText(/phoebe look for (.+)/i, lookfor);
 
 bot.on("message", (msg) => {
   const chatId = msg.chat.id;
 
   // send a message to the chat acknowledging receipt of their message
+  console.log(msg.text);
   // bot.sendMessage(chatId, "Received your message");
 });
 
