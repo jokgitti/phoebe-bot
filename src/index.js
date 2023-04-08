6fe7d036df2e83774d5baaeabcce3e72f282d764
Massimiliano D'Elia <dev@maxdelia.it>
2023-04-08 22:35:50 +0200

    feat: add Dotenv and Pino logger

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-import { getTelegramBot } from "./telegramBot.js";
-import { lookAgain, lookfor } from "./text/lookfor.js";
+import { bingSearchApiKey, telegramApiKey } from "./config/index.js"
+import logger, { censor } from "./helpers/logger.js"
+import { getTelegramBot } from "./telegramBot.js"
+import { lookAgain, lookfor } from "./text/lookfor.js"
 
-console.log(process.env.TELEGRAM_API_KEY);
-console.log(process.env.BING_SEARCH_API_KEY);
+if (!telegramApiKey) {
+  logger.fatal(`Invalid TELEGRAM_API_KEY value: ${telegramApiKey}`)
+  process.exit(1)
+}
 
-const telegramBot = getTelegramBot();
+logger.debug(`BING_SEARCH_API_KEY=${censor(bingSearchApiKey)}`)
+logger.debug(`TELEGRAM_API_KEY=${censor(telegramApiKey)}`)
 
-telegramBot.onText(/phoebe look for (.+)/i, lookfor);
-telegramBot.onText(/phoebe look again/i, lookAgain);
+const telegramBot = getTelegramBot()
+
+telegramBot.onText(/phoebe look for (.+)/i, lookfor)
+telegramBot.onText(/phoebe look again/i, lookAgain)
 
 telegramBot.on("message", (msg) => {
-  console.log("message received", msg);
-});
+  logger.debug({ msg }, "message received")
+})
 
-telegramBot.on("polling_error", (error) => {
-  console.log(error);
-});
+telegramBot.on("polling_error", (err) => {
+  logger.error({ err }, "polling error")
+})
