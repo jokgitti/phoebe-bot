4ea79c08953ea4a991df5a6772e5e9d29f48431b
Massimiliano D'Elia <dev@maxdelia.it>
2023-04-08 23:46:33 +0200

    fix: incoming message not logged

--- src/index.js
+++ src/index.js
@@ -16,8 +16,20 @@ const telegramBot = getTelegramBot()
 telegramBot.onText(/phoebe look for (.+)/i, lookfor)
 telegramBot.onText(/phoebe look again/i, lookAgain)
 
-telegramBot.on("message", (msg) => {
-  logger.debug({ msg }, "message received")
+telegramBot.on("message", ({ chat, date, from, message_id, text }) => {
+  logger.debug(
+    {
+      chat_id: chat.id,
+      chat_title: chat.title,
+      chat_type: chat.type,
+      from_id: from.id,
+      from_username: from.username,
+      message_id: message_id,
+      message_date: date,
+      message_text: text,
+    },
+    "message received"
+  )
 })
 
 telegramBot.on("polling_error", (err) => {
