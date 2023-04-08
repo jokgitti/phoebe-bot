b6f8081a7212a797d1c2c821ea4296e5fb578686
Luca Galeazzi <luca.galeazzi@jimdo.com>
2023-04-08 22:22:11 +0200

    add openai integration to generate caption for images

---
+++ src/openAi.js
@@ -0,0 +1,16 @@
+import { Configuration, OpenAIApi } from "openai"
+
+import { openaiApiKey } from "./config/index.js"
+
+const openAIConfig = new Configuration({
+  apiKey: openaiApiKey,
+})
+
+let openAI = null
+
+export const getOpenAI = () => {
+  if (!openAI) {
+    openAI = new OpenAIApi(openAIConfig)
+  }
+  return openAI
+}
