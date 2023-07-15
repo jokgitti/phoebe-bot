6d26144ca61dfdd246c474ba65152b266d5f4750
Massimiliano D'Elia <dev@maxdelia.it>
2023-07-15 15:24:41 +0200

    chore: linting

--- src/services/inspireMeBot.js
+++ src/services/inspireMeBot.js
@@ -1,14 +1,14 @@
 import request from "request"
 
-const INSPIRE_ME_BOT_API_URL = 'https://inspirobot.me/api?generate=true'
+const INSPIRE_ME_BOT_API_URL = "https://inspirobot.me/api?generate=true"
 
 export const inspireMeBot = () => {
-    return new Promise((resolve, reject) => {
-        request(INSPIRE_ME_BOT_API_URL, (error, _, body) => {
-            if (error) {
-                return reject(error)
-            }
-            resolve(body)
-        })
+  return new Promise((resolve, reject) => {
+    request(INSPIRE_ME_BOT_API_URL, (error, _, body) => {
+      if (error) {
+        return reject(error)
+      }
+      resolve(body)
     })
+  })
 }
--- src/text/inspireme.js
+++ src/text/inspireme.js
@@ -5,13 +5,13 @@ import { getTelegramBot } from "../telegramBot.js"
 const telegramBot = getTelegramBot()
 
 export const inspireMe = async (msg) => {
-    try {
-        const result = await inspireMeBot()
-        console.log(result)
-        await telegramBot.sendPhoto(msg.chat.id, result)
-    } catch (err) {
-        console.log(err)
-        logger.error({ err })
-        await telegramBot.sendMessage(msg.chat.id, "I couldn't get inspired :(")
-    }
-} 
+  try {
+    const result = await inspireMeBot()
+    console.log(result)
+    await telegramBot.sendPhoto(msg.chat.id, result)
+  } catch (err) {
+    console.log(err)
+    logger.error({ err })
+    await telegramBot.sendMessage(msg.chat.id, "I couldn't get inspired :(")
+  }
+}
