ab3653f00eba969eb89a9d208a57368d6c4b46aa
Luca Galeazzi <jokgitti@gmail.com>
2025-01-09 11:38:52 +0100

    feat: add explain command

---
+++ src/text/explain.js
@@ -0,0 +1,16 @@
+import { getTelegramBot } from "../telegramBot.js"
+import pollinationsAI from "../services/pollinationsAI.js"
+
+export async function explain(msg, match) {
+    const telegramBot = getTelegramBot()
+    const query = match[1]
+
+    try {
+        const res = await pollinationsAI.text(query)
+        await telegramBot.sendMessage(msg.chat.id, res)
+
+    } catch (error) {
+        telegramBot.sendMessage(msg.chat.id, 'scusa non ho capito')
+    }
+
+}
