f32e796a882d7a9783e2f90197f144361748282a
Luca Galeazzi <jokgitti@gmail.com>
2025-01-09 11:38:52 +0100

    feat: add explain command

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { withAdminAuth, withAuth } from "./helpers/auth.js"
 import logger, { censor } from "./helpers/logger.js"
 import { getTelegramBot } from "./telegramBot.js"
 import { listAdmins, listUsers } from "./text/auth.js"
+import { explain } from "./text/explain.js"
 import { help, whoami } from "./text/help.js"
 import { inspireMe } from "./text/inspireme.js"
 import { lookfor, undo } from "./text/lookfor.js"
@@ -28,6 +29,7 @@ telegramBot.onText(/phoebe undo/i, withAuth(undo))
 telegramBot.onText(/phoebe inspire me/i, withAuth(inspireMe))
 telegramBot.onText(/phoebe help/i, help)
 telegramBot.onText(/phoebe whoami/i, whoami)
+telegramBot.onText(/phoebe explain (.+)/i, withAuth(explain))
 
 telegramBot.on("message", (msg) => {
   logger.debug({ ...msg }, "incoming message")
