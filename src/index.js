c36192db4f6742331d760ed098f8799d7427f3ba
Luca Galeazzi <jokgitti@gmail.com>
2025-01-09 12:11:51 +0100

    feat: add generate command

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import logger, { censor } from "./helpers/logger.js"
 import { getTelegramBot } from "./telegramBot.js"
 import { listAdmins, listUsers } from "./text/auth.js"
 import { explain } from "./text/explain.js"
+import { generate } from "./text/generate.js"
 import { help, whoami } from "./text/help.js"
 import { inspireMe } from "./text/inspireme.js"
 import { lookfor, undo } from "./text/lookfor.js"
@@ -30,6 +31,7 @@ telegramBot.onText(/phoebe inspire me/i, withAuth(inspireMe))
 telegramBot.onText(/phoebe help/i, help)
 telegramBot.onText(/phoebe whoami/i, whoami)
 telegramBot.onText(/phoebe explain (.+)/i, withAuth(explain))
+telegramBot.onText(/phoebe generate (.+)/i, withAuth(generate))
 
 telegramBot.on("message", (msg) => {
   logger.debug({ ...msg }, "incoming message")
