f1010bebceb695794bcfca0abf8cc1e914f1603f
Massimiliano D'Elia <dev@maxdelia.it>
2025-01-15 22:05:10 +0100

    chore: formatting

--- src/text/kawaii.js
+++ src/text/kawaii.js
@@ -1,22 +1,20 @@
-import { getTelegramBot } from "../telegramBot.js";
+import { getTelegramBot } from "../telegramBot.js"
 import pollinationsAI from "../services/pollinationsAI.js"
 
 export async function kawaii(msg, match) {
-    const telegramBot = getTelegramBot()
-    const query = match[1]
+  const telegramBot = getTelegramBot()
+  const query = match[1]
 
-    try {
-        const res = await pollinationsAI.image(`an anime version of ${query}`, 'flux-anime')
-        const arrayBuffer = await res.arrayBuffer();
-        const buffer = Buffer.from(arrayBuffer);
+  try {
+    const res = await pollinationsAI.image(`an anime version of ${query}`, "flux-anime")
+    const arrayBuffer = await res.arrayBuffer()
+    const buffer = Buffer.from(arrayBuffer)
 
-        await telegramBot.sendPhoto(msg.chat.id, buffer, {
-            parse_mode: "HTML",
-        })
-
-    } catch (error) {
-        console.error(error)
-        telegramBot.sendMessage(msg.chat.id, 'めちゃくちゃににゃっちゃった ≽^•⩊•^≼')
-    }
-
+    await telegramBot.sendPhoto(msg.chat.id, buffer, {
+      parse_mode: "HTML",
+    })
+  } catch (error) {
+    console.error(error)
+    telegramBot.sendMessage(msg.chat.id, "めちゃくちゃににゃっちゃった ≽^•⩊•^≼")
+  }
 }
