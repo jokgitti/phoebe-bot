0f4eb9ce105a87bf46dc95b0bce9086b22d30297
ProvoK <vittorio.camisa@gmail.com>
2025-06-26 15:47:49 +0200

    feat: un perticone e' per sempre

--- src/index.js
+++ src/index.js
@@ -1,44 +1,50 @@
-import { bingSearchApiKey, telegramApiKey, openaiApiKey } from "./config/index.js"
-import { withAdminAuth, withAuth } from "./helpers/auth.js"
-import logger, { censor } from "./helpers/logger.js"
-import { getTelegramBot } from "./telegramBot.js"
-import { listAdmins, listUsers } from "./text/auth.js"
-import { explain } from "./text/explain.js"
-import { generate } from "./text/generate.js"
-import { help, whoami } from "./text/help.js"
-import { inspireMe } from "./text/inspireme.js"
-import { kawaii } from "./text/kawaii.js"
-import { lookfor, undo } from "./text/lookfor.js"
+import {
+	bingSearchApiKey,
+	telegramApiKey,
+	openaiApiKey,
+} from "./config/index.js";
+import { withAdminAuth, withAuth } from "./helpers/auth.js";
+import logger, { censor } from "./helpers/logger.js";
+import { getTelegramBot } from "./telegramBot.js";
+import { listAdmins, listUsers } from "./text/auth.js";
+import { explain } from "./text/explain.js";
+import { generate } from "./text/generate.js";
+import { help, whoami } from "./text/help.js";
+import { inspireMe } from "./text/inspireme.js";
+import { kawaii } from "./text/kawaii.js";
+import { lookfor, undo } from "./text/lookfor.js";
+import { thePertiGame } from "./test/perticottero.js";
 
 if (!telegramApiKey) {
-  logger.fatal(`Invalid TELEGRAM_API_KEY value: ${telegramApiKey}`)
-  process.exit(1)
+	logger.fatal(`Invalid TELEGRAM_API_KEY value: ${telegramApiKey}`);
+	process.exit(1);
 }
 
 logger.debug({
-  bingSearchApiKey: censor(bingSearchApiKey),
-  openaiApiKey: censor(openaiApiKey),
-  telegramApiKey: censor(telegramApiKey),
-})
+	bingSearchApiKey: censor(bingSearchApiKey),
+	openaiApiKey: censor(openaiApiKey),
+	telegramApiKey: censor(telegramApiKey),
+});
 
-const telegramBot = getTelegramBot()
+const telegramBot = getTelegramBot();
 
-telegramBot.onText(/phoebe list admins/i, withAdminAuth(listAdmins))
-telegramBot.onText(/phoebe list users/i, withAdminAuth(listUsers))
-telegramBot.onText(/phoebe look for (.+)/i, withAuth(lookfor))
-telegramBot.onText(/phoebe look again/i, withAuth(lookfor))
-telegramBot.onText(/phoebe undo/i, withAuth(undo))
-telegramBot.onText(/phoebe inspire me/i, withAuth(inspireMe))
-telegramBot.onText(/phoebe help/i, help)
-telegramBot.onText(/phoebe whoami/i, whoami)
-telegramBot.onText(/phoebe explain (.+)/i, withAuth(explain))
-telegramBot.onText(/phoebe generate (.+)/i, withAuth(generate))
-telegramBot.onText(/phoebe kawaii (.+)/i, withAuth(kawaii))
+telegramBot.onText(/phoebe list admins/i, withAdminAuth(listAdmins));
+telegramBot.onText(/phoebe list users/i, withAdminAuth(listUsers));
+telegramBot.onText(/phoebe look for (.+)/i, withAuth(lookfor));
+telegramBot.onText(/phoebe look again/i, withAuth(lookfor));
+telegramBot.onText(/phoebe undo/i, withAuth(undo));
+telegramBot.onText(/phoebe inspire me/i, withAuth(inspireMe));
+telegramBot.onText(/phoebe help/i, help);
+telegramBot.onText(/phoebe whoami/i, whoami);
+telegramBot.onText(/phoebe explain (.+)/i, withAuth(explain));
+telegramBot.onText(/phoebe generate (.+)/i, withAuth(generate));
+telegramBot.onText(/phoebe kawaii (.+)/i, withAuth(kawaii));
+telegramBot.onText(/phoebe (.+)perticone(.+)/i, withAuth(thePertiGame));
 
 telegramBot.on("message", (msg) => {
-  logger.debug({ ...msg }, "incoming message")
-})
+	logger.debug({ ...msg }, "incoming message");
+});
 
 telegramBot.on("polling_error", (err) => {
-  logger.error({ err }, "polling error")
-})
+	logger.error({ err }, "polling error");
+});
