fcf004ca67300fe4ad6358111a473916f0062de3
Massimiliano D'Elia <dev@maxdelia.it>
2026-04-12 20:37:20 +0200

    feat: remove the broken request made by the explain command

--- src/commands/explain.js
+++ src/commands/explain.js
@@ -1,15 +1,5 @@
-import logger from "../helpers/logger.js"
-import { GENERIC_ERROR } from "../helpers/replies.js"
-import pollinationsAI from "../services/pollinationsAI.js"
+import { WIP } from "../helpers/replies.js"
 
 export async function explain(ctx) {
-  const query = ctx.match[1]
-
-  try {
-    const { response } = await pollinationsAI.text(query)
-    await ctx.reply(response.replace(/^"|"$/g, ""))
-  } catch (error) {
-    logger.error({ err: error })
-    await ctx.reply(GENERIC_ERROR)
-  }
+  await ctx.reply(WIP)
 }
